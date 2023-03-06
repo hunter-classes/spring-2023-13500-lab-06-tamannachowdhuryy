105705f0cbb91fcac3fe2a60aa3fce3c451e89d9
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2023-03-06 13:21:58 -0500

    completed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
-
 #include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 int main()
 {
+  std::cout << "Task B: encrptCaesar(\"Way to Go!\", 5) = " << encryptCaesar("Way to Go!", 5) << std::endl;
+  std::cout << "Task C: encryptVigenere(\"Hello, World!\", \"cake\") = " << encryptVigenere("Hello, World!", "cake") << std::endl;
+  std::cout << "Task D: decryptCaesar(Bfd yt Lt!, 5) = " << decryptCaesar("Bfd yt Lt!", 5) << std::endl;
+  std::cout << "Task D: decryptVigenere(Jevpq, Wyvnd!, 5) = " << decryptVigenere("Jevpq, Wyvnd!", "cake") << std::endl;
   return 0;
 }
+
