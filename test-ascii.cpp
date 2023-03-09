afe2f482e7994fb0459a5a1613a747b7255c9fb4
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2023-03-08 22:37:15 -0500

    update

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -3,7 +3,7 @@ Author: Tamanna Chowdhury
 Course: CSCI-135
 Instructor: Mike Zamansky
 Assignment: Lab6A
-This program asks the user to input a line of text (which may possibly include spaces). Then, it reports all the characters from the input line together with their ASCII codes.
+This program asks the user to input a line of text (which may possibly include spaces). Then, it reports all the characters from the input line together with their ASCII codes
 */
 
 #include <iostream>
