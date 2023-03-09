3c5f8c9ed0cc6aa4789b91bb4793b8e1ddcc7903
Tamanna Chowdhury <tamanna.chowdhury610@gmail.com>
2023-03-08 19:13:48 -0500

    update

--- tests.cpp
+++ tests.cpp
@@ -4,23 +4,19 @@
 #include "vigenere.h"
 #include "decrypt.h"
 
-TEST_CASE("shiftChar"){
-  CHECK(shiftChar('W', 5) == 'B');
-  CHECK(shiftChar('o', 10) == 'y');
-  CHECK(shiftChar('!', 15) == '!');
-}
 
-TEST_CASE("Caesar Cipher Encryption"){
+//TASK B
+TEST_CASE("TASK B: Caesar Cipher Encryption"){
 CHECK(encryptCaesar("Hello, World!", 10) == "Rovvy, Gybvn!");
 CHECK(encryptCaesar("Way to Go!", 5)== "Bfd yt Lt!");
 }
-
-TEST_CASE("Vigenere Cipher Encryption"){
+//TASK C
+TEST_CASE("TASK C: Vigenere Cipher Encryption"){
 CHECK(encryptVigenere("Hello, World!","cake") == "Jevpq, Wyvnd!");
 CHECK(encryptVigenere("Jackdaws love my big sphinx of quartz", "amazing") == "Jmcjlncs xoum ze bug rxuonj oe yhgrfz");  
 }
-
-TEST_CASE("Decryption"){
+//TASK D
+TEST_CASE("TASK D: Decryption"){
 CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
 CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
 }
